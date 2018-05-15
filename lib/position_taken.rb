9a27d3ec581dd5f5485c7377e28190cfba007843
Julie <juliejhudspeth@gmail.com>
2018-05-15 19:11:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board,index)
-  if board == " " || board == "" || board == "nil"
+  if board == " " || board == "" || board == nil
     false
   elsif board == "X" || "O"
     true
